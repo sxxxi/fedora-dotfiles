5fd490d02d549ddaea0fc139621101ca4404103a
Seiji <akakabeseiji0@gmail.com>
2025-01-12 22:41:47 +0900

    oops

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ fi;
 
 # Node Version Manager
 if [ ! -s $HOME/.nvm ]; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | zsh;
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash;
 fi;
 
 # Setup git key
